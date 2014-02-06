2934fdb8a117024842251b0a009249b338be6915
Jacob Gable <jacob.gable@gmail.com>
2014-02-05 20:11:27 -0600

    Setup example of source maps being off

---
+++ demo/source1.js
@@ -0,0 +1,23 @@
+
+import { hello } from 'test1';
+
+export var things = {
+	'1': 0,
+	'2': 0,
+	'3': 0,
+
+	updateThing: function (name, val) {
+		this[name] = val;
+
+		if (name === 'bingo') {
+			console.log('bingo was his name o');
+
+			this[name] += ' was his name o';
+		}
+	}
+
+};
+
+export default {
+	things: things
+};
