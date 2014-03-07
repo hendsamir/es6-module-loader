87a8f6e9dc3fda28be765bf56d4079e4bce746e5
Jacob Gable <jacob.gable@gmail.com>
2014-03-07 11:44:37 -0600

    Adding demo files

--- demo/source1.js
+++ demo/source1.js
@@ -2,22 +2,22 @@
 import { hello } from 'test1';
 
 export var things = {
-	'1': 0,
-	'2': 0,
-	'3': 0,
+    '1': 0,
+    '2': 0,
+    '3': 0,
 
-	updateThing: function (name, val) {
-		this[name] = val;
+    updateThing: function (name, val) {
+        this[name] = val;
 
-		if (name === 'bingo') {
-			console.log('bingo was his name o');
+        if (name === 'bingo') {
+            console.log('bingo was his name o');
 
-			this[name] += ' was his name o';
-		}
-	}
+            this[name] += ' was his name o';
+        }
+    }
 
 };
 
 export default {
-	things: things
+    things: things
 };
